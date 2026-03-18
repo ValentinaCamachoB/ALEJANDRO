5530b3d9d7cddce44bc5172676eba1c495770c96
Valentina Camacho <Valencamacho777@hotmail.com>
2026-03-18 07:36:42 -0500

    Clima 2

--- ALEJANDRO/Clima/scripts/persistence.js
+++ ALEJANDRO/Clima/scripts/persistence.js
@@ -1,15 +1,15 @@
 
 //  interactuar con localstorage
- 
+
 export function setLocalStorageValue(key, value) {
     localStorage.setItem(key, JSON.stringify(value));
 }
- 
+
 export function getLocalStorageValue(key) {
     return JSON.parse(localStorage.getItem(key));
 }
- 
+
 export function clearLocalStorage() {
     localStorage.clear();
 }
- 
+
